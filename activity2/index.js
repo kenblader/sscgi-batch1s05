f0b676b54fad9a7a51e48b22a5506fa1ad386f52
Kenshiro Nonan <kmnonan@sscgi.com>
2024-11-27 16:24:14 +0800

    Update activity 2.0

--- activity2/index.js
+++ activity2/index.js
@@ -2,8 +2,8 @@
 let form = document.getElementById("pasaLoadForm");
 let loginContainer = document.getElementById("loginContainer");
 let addAmount = document.getElementById("addAmountContainer");
-let modal1 = new bootstrap.Modal(document.getElementById('addAmountContainer'));
-
+let registerContainer = document.getElementById("registerContainer");
+let modal1 = new bootstrap.Modal(document.getElementById("addAmountContainer"));
 
 let amountAvailable = 0;
 let userMobileNo;
@@ -13,55 +13,69 @@ let counter = 0;
 
 let invoice = [];
 showForm();
+// ============== test ===============
+// let user = [];
+// {
+//   mobileNum,
+//   Amount,
+//   history,
+// }
 
 // ============== Events ===============
-document.getElementById("resendOTP").addEventListener("click", () =>{
+document.getElementById("resendOTP").addEventListener("click", () => {
   randNum = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-  console.log(randNum)
-})
+  console.log(randNum);
+});
 
 document.getElementById("otpBtn").addEventListener("click", () => {
   let otpSection = document.getElementById("block1");
   let addAmountSection = document.getElementById("block2");
   let otpInput = document.getElementById("otpValidation");
 
-  if(randNum == otpInput.value){
+  if (randNum == otpInput.value) {
     otpSection.style.display = "none";
     addAmountSection.style.display = "block";
   } else {
     alert("Wrong otp");
     document.getElementById("otpValidation").value = "";
   }
-})
+});
 
 document.getElementById("loginBtn").addEventListener("click", () => {
-  userMobileNo = document.getElementById("mobileNo").value;
+  userMobileNo = document.getElementById("loginMobileNo").value;
   //console.log(userMobileNo.length);
-  let isTrue= validateMobileNumberInput(userMobileNo, userMobileNo.length);
+  let isTrue = validateMobileNumberInput(userMobileNo, userMobileNo.length);
 
-  document.getElementById("mobileNo").value = "";
-  //amountAvailable = document.getElementById("amountAvail").value;
-  if(isTrue != false){
+  document.getElementById("loginMobileNo").value = "";
+  if (isTrue != false) {
     counter = 1;
     formProcess();
   }
+
+  // for(elements of user){
+  //   if(elements === user.userMobileNumber){
+  //     document.getElementById("loginMobileNo").value = "";
+  //     counter = 1;
+  //     formProcess();
+  //   }
+  // }
 });
 
 document.getElementById("logoutBtn").addEventListener("click", () => {
   location.reload();
 });
 
-document.getElementById('addLoad').addEventListener("click", function() {
+document.getElementById("addLoad").addEventListener("click", function () {
   showAmount(); // Opens or closes the modal
 });
 
 document.getElementById("submitBtn").addEventListener("click", () => {
   let mobileToSend = document.getElementById("mobileNumToSend").value;
   amountToSend = document.getElementById("amountToSend").value;
-  
+
   let isTrue = validateMobileNumberInput(mobileToSend, mobileToSend.length);
 
-  if(isTrue != false){
+  if (isTrue != false) {
     pasaloadProcess(amountToSend);
   }
 });
@@ -75,12 +89,32 @@ document.getElementById("submitAddBtn").addEventListener("click", () => {
 
   let amountAdded = document.getElementById("amountToAdd").value;
   document.getElementById("amountToAdd").value = "";
-  amountAvailable =  Number(amountAvailable) + Number(amountAdded);
+  amountAvailable = Number(amountAvailable) + Number(amountAdded);
 
-  let addHistory = Number(amountAdded)+ " amount was added";
+  let addHistory = Number(amountAdded) + " amount was added";
   invoice.push(addHistory);
   formProcess();
 });
+
+// document.getElementById("signUp").addEventListener("click", () => {
+//   showSignUp(true);
+// })
+
+// document.getElementById("signUpBtn").addEventListener("click", () => {
+//   let registerUser = document.getElementById("registerMobileNo").value;
+
+//   let isTrue= validateMobileNumberInput(registerUser, registerUser.length);
+
+//   if(isTrue != false){
+//     user = {
+//       userMobileNumber: registerUser,
+//       loadBalance: 0,
+//       invoice: []
+//     }
+
+//     showSignUp(false);
+//   }
+// })
 
 // ============== Functions ===============
 
@@ -93,25 +127,36 @@ function formProcess() {
   printHistory();
 }
 
-function pasaloadProcess(amount, ) {
+function pasaloadProcess(amount) {
   if (amountAvailable < amount) {
     insufficientBalance();
   } else {
     amountAvailable = Number(amountAvailable) - Number(amount);
-    let addHistory = Number(amount)+ " amount was deducted";
+    let addHistory = Number(amount) + " amount was deducted";
     invoice.push(addHistory);
     document.getElementById("amountAvailable").innerHTML = amountAvailable;
 
     document.getElementById("mobileNumToSend").value = "";
     document.getElementById("amountToSend").value = "";
-    printHistory()
+    printHistory();
   }
 }
 
 function insufficientBalance() {
-  alert("Insufficient Load Amount");  
+  alert("Insufficient Load Amount");
   showAmount();
 }
+
+// function showSignUp(check) {
+//   if(check){
+//     registerContainer.style.display ="block";
+//     loginContainer.style.display = "none";
+//   }else{
+//     registerContainer.style.display ="none";
+//     loginContainer.style.display = "block";
+//   }
+
+// }
 
 function showForm() {
   if (counter == "1") {
@@ -120,21 +165,21 @@ function showForm() {
     addAmount.style.display = "none";
   } else {
     form.style.display = "none";
-    // addAmount.style.display = "none";
+    // registerContainer.style.display ="none";
   }
 }
 
 function showAmount() {
   let addAmountSection = document.getElementById("block2");
   randNum = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-  console.log(randNum)
+  console.log(randNum);
 
   modal1.show();
   addAmountSection.style.display = "none";
 }
 
 function validateMobileNumberInput(mobileNo, length) {
-  if(mobileNo == "" || length != 11){
+  if (mobileNo == "" || length != "11") {
     alert("Mobile Number must be filled out and it should be 11 digits");
     return false;
   }
@@ -142,14 +187,10 @@ function validateMobileNumberInput(mobileNo, length) {
 
 function printHistory() {
   const ulTag = document.getElementById("historyList");
-  const newDiv = document.createElement('div');
+  const newDiv = document.createElement("div");
 
-  for(let elements of invoice){
+  for (let elements of invoice) {
     newDiv.innerHTML = elements;
     ulTag.appendChild(newDiv);
   }
 }
-
-
-
-
